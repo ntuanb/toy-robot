cc3be3ae7eab1fb8519fd6b64337a9ae03bfb517
Tuan Bui <ntuanb@gmail.com>
2020-04-16 22:10:40 +1000

    Add ability to move east, south and west

--- src/move.js
+++ src/move.js
@@ -1,7 +1,7 @@
+const STEP_SIZE = 1;
+
 module.exports = {
-  move(position) {
-    const STEP_SIZE = 1;
-  
+  move(position) {  
     let x = position.x;
     let y = position.y;
     let face = position.face;
@@ -15,6 +15,37 @@ module.exports = {
         } else if (y < 0) {
           y = 0;
         }
+        break;
+      case 'EAST':
+        x += STEP_SIZE;
+
+        if (x > 5) {
+          x = 5;
+        } else if (x < 0) {
+          x = 0;
+        }
+        
+        break;
+      case 'SOUTH':
+        y -= STEP_SIZE;
+        
+        if (y > 5) {
+          y = 5;
+        } else if (y < 0) {
+          y = 0;
+        }
+
+        break;
+      case 'WEST':
+        x -= STEP_SIZE;
+        
+        if (x > 5) {
+          x = 5;
+        } else if (x < 0) {
+          x = 0;
+        }
+
+        break;
     }
   
     return {
